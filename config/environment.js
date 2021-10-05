0401c3dea40d647b1a6ceccca4c2ec957c20a496
Bill Dami <billdami@gmail.com>
2021-10-05 12:08:08 -0400

    installing core 3rd party addons

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ember-v4-test',
+    podModulePrefix: 'ember-v4-test/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
