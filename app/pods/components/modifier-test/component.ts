d26eee08f87cb0fe3500ff3945310492624249c6
Bill Dami <billdami@gmail.com>
2021-10-05 15:22:50 -0400

    installing core 3rd party addons

--- app/pods/components/modifier-test/component.ts
+++ app/pods/components/modifier-test/component.ts
@@ -1,9 +1,18 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import NotificationService from 'ember-cli-notifications/services/notifications';
+import ENV from 'ember-v4-test/config/environment';
 
 interface ModifierTestArgs {}
 
 export default class ModifierTest extends Component<ModifierTestArgs> {
+    @service notifications!: NotificationService;
+
+    testVar = ENV.testVar;
+
+    @action
     onDidInsert() {
-        console.log('I was inserted in the DOM!');
+        this.notifications.success('<ModifierTest /> was inserted in the DOM!');
     }
 }
